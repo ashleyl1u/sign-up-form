8b6098663d387dc09f862fc2d05960d8da08c5bb
Ashley Liu <ashleyl1u095@gmail.com>
2024-06-25 12:02:07 -0400

    Add styles to page

--- main.js
+++ main.js
@@ -24,20 +24,33 @@ function confirmPassword(){
   const confirmPasswordError = document.querySelector('.confirm-password-error');
   
   if((confirmPassword.value !== password.value) || password.value ===''){
-    password.classList.remove('valid');
-    confirmPassword.classList.remove('valid');
     password.classList.add('invalid');
     confirmPassword.classList.add('invalid');
     confirmPasswordError.textContent = '*Passwords do not match'
+   
     return false;
   }
   else{
     password.classList.remove('invalid');
     confirmPassword.classList.remove('invalid');
-    password.classList.add('valid');
-    confirmPassword.classList.add('valid');
     confirmPasswordError.textContent = ''
+
     return true;
   }
 }
 
+
+
+document.querySelectorAll('.input').forEach((element) => {
+  element.addEventListener('focus',() => {
+    const inputId = element.getAttribute('id');
+    document.querySelector(`.${inputId}-label`).style.color = '#536878';
+  });
+});
+
+document.querySelectorAll('.input').forEach((element) => {
+  element.addEventListener('focusout',() => {
+    const inputId = element.getAttribute('id');
+    document.querySelector(`.${inputId}-label`).style.color = 'black';
+  });
+});
